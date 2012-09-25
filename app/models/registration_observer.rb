953012c0cea51de6838308f6508f712513c14e7a
Boris Raeschler <boris.raeschler@gmail.com>
2012-09-25 13:59:01 +0200

    changed mail, event date is now dynamic. Added payment informations

--- app/models/registration_observer.rb
+++ app/models/registration_observer.rb
@@ -2,6 +2,6 @@ class RegistrationObserver < ActiveRecord::Observer
   
    def after_create(registration)
      RegistrationMailer.deliver_registration_email(registration)
-     RegistrationMailer.deliver_registration_team_email(registration)
+     RegistrationMailer.deliver_registration_team_email(registration) unless RAILS_ENV == "development"
    end
 end
