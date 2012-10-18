fff8509f516646dc8665b9af9cc8de80b61ee369
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2012-10-18 10:05:40 +0200

    routes for prices and categories in admin interface added. Prices and Categories List will be displayed in the admin interface. Copy-Paste Errors in the Interfaces fixed. Model for categories updated to reflect a category tree

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,10 @@
 class Category < ActiveRecord::Base
 
-    has_many :prices
+    # Set up a category tree: each #category can belong to one root category, but have many branch categories
+    belongs_to :category
+	has_many :categories
+    # other resources that will be categorized - many to many relationship for now...
+    has_and_belongs_to_many :events
+    has_and_belongs_to_many :prices
 
 end
