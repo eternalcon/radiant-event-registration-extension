e1496cd6bf75b296066e743ea30b9e659564a8c9
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2012-10-19 08:20:47 +0200

    made categories polymorphic

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,7 @@
 class Category < ActiveRecord::Base
 
-    # Set up a category tree: each #category can belong to one root category, but have many branch categories
-    belongs_to :category
-	has_many :categories
-    # other resources that will be categorized - many to many relationship for now...
-    has_and_belongs_to_many :events
-    has_and_belongs_to_many :prices
+    # make categories polymorphic and self-referential
+    belongs_to :categorizable, polymorphic => true
+	has_many :categories, as => :categorizable
 
 end
