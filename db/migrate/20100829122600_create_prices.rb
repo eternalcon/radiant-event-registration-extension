6465e5bbbecd76fb78531037e0f9a4cccfd8f3c2
Daniel Zaumsegel <daniel.zaumsegel@gmail.com>
2012-10-28 10:04:26 +0100

    modified original prices migration to include the polymorphic foreign key column plus type column

--- db/migrate/20100829122600_create_prices.rb
+++ db/migrate/20100829122600_create_prices.rb
@@ -1,16 +1,16 @@
 class CreatePrices < ActiveRecord::Migration
-  def self.up
-    create_table :prices do |t|
-      t.string :name
-      t.decimal :price
-      t.date :valid_from
-      t.date :valid_until
-      t.references :category
-      t.timestamps
+    def self.up
+        create_table :prices do |t|
+            t.string :name
+            t.decimal :price
+            t.date :valid_from
+            t.date :valid_until
+            t.integer :pricable_id
+            t.string :pricable_type
+            t.timestamps
+        end
     end
-  end
-
-  def self.down
-    drop_table :prices
-  end
+    def self.down
+        drop_table :prices
+    end
 end
